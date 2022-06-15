c1996cecccdff1e143797e7292f7ee4865255d5e
Joao Marin Jr <joaomarinjr@hotmail.com>
2022-06-15 01:09:06 -0300

    Migrando para Extension library

---
+++ App_Data/Projects/PhoneRepairShop_Code/PhoneRepairShop_Code/RSSVRepairServiceMaint.cs
@@ -0,0 +1,64 @@
+using System;
+using PX.Data;
+using PX.Data.BQL.Fluent;
+
+namespace PhoneRepairShop
+{
+    public class RSSVRepairServiceMaint : PXGraph<RSSVRepairServiceMaint>
+    {
+
+        protected void RSSVRepairService_WalkInService_FieldUpdated(PXCache cache, PXFieldUpdatedEventArgs e)
+        {
+
+            var row = (RSSVRepairService)e.Row;
+
+            if (row.WalkInService == true)
+            {
+                row.PreliminaryCheck = false;
+            }
+            else
+            {
+                row.PreliminaryCheck = true;
+            }
+
+        }
+
+        protected void _(Events.FieldUpdated<RSSVRepairService,RSSVRepairService.preliminaryCheck> e)
+        {
+            var row = e.Row;
+            if (row.PreliminaryCheck == true)
+            {
+                row.WalkInService = false;
+            }
+            else
+            {
+                row.WalkInService = true;
+            }
+        }
+
+
+
+        public SelectFrom<RSSVRepairService>.View RepairService;
+
+        public PXSave<RSSVRepairService> Save;
+        public PXCancel<RSSVRepairService> Cancel;
+
+
+        public PXFilter<MasterTable> MasterView;
+        public PXFilter<DetailsTable> DetailsView;
+
+        [Serializable]
+        public class MasterTable : IBqlTable
+        {
+
+        }
+
+        [Serializable]
+        public class DetailsTable : IBqlTable
+        {
+
+        }
+
+
+    }
+}
---
+++ App_Data/Projects/PhoneRepairShop_Code/Solution.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Acumatica ERP\PhoneRepairShop\App_Data\Projects\PhoneRepairShop_Code\PhoneRepairShop_Code.sln"
